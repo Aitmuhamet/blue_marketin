e0ffdd7314ef08093a06effa99b584e8c688b3fe
Aitmuhamet <xa210296@gmail.com>
2022-05-12 19:00:54 +0500

    12 may

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ var div = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function(){
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  // var dY = scrolled - oldScrollY;
+  var dY = scrolled - oldScrollY;
   
-  if ( scrolled > 0 ){
-  div.classList.add('navbar-top-fixed');
-  } else {
-  div.classList.remove('navbar-top-fixed');
+  if ( dY <= 0 ){
+    div.classList.add('navbar-top-fixed');
+  } else if ( dY > 0 || scrolled == 0 ) {
+    div.classList.remove('navbar-top-fixed');
+    div.classList.add('navbar-to-top');
   }
+  oldScrollY = scrolled;
   
+  console.log(scrolled);
   // oldScrollY = scrolled;
 });
